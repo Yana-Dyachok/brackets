b60e8dc4a91bc647751c7c890f37dc3b63553b8d
Yana-Dyachok <97878430+Yana-Dyachok@users.noreply.github.com>
2023-02-14 17:25:56 +0200

    feat: add my code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 !== 0) return false
+  let stack = []
+  let brConfigObj = Object.fromEntries(bracketsConfig)
+ for (let i = 0; i < str.length; i++) {
+  if(brConfigObj[stack[stack.length - 1]]=== str[i]) stack.pop() 
+  else(stack.push(str[i]))
+ }
+ return  stack.length===0;
 }
